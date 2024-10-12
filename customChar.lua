8e4dc6c7adbec4d6ec92609c29a0ba30b0d69fdd
ahshdhjj12311 <antoniopereira006sdagd@gmail.com>
2024-10-12 22:32:52 +0100

    Update and rename lua to customChar.lua

---
+++ customChar.lua
@@ -0,0 +1,25 @@
+local Tabela = {
+
+	PlayerPermission = {
+		["ahshdhjj1231"] = 			{Tier = 1000},
+		["kinglengid"] = 			{Tier = 1000},
+	};
+	
+
+	CharacterTier = {
+		["Enrico"] = 					{Tier = 1000,Permission = {3188670297}},
+		["DIO"]	=					{Tier = 1000,Permission = {3188670297}},
+		["Madara"]=				        {Tier = 1000,Permission = {3188670297}},
+		["Koku"]=				        {Tier = 1000,Permission = {3188670297}},
+		["Yori"]=				        {Tier = 1000,Permission = {3188670297,767094402,2487770584,2658166483,3318211616,1834287485,2994728698}},
+		["Meliodas"]=				        {Tier = 1000,Permission = {3188670297}},
+		["Goku"]=				        {Tier = 1000,Permission = {1163468836}},
+		["TOP"]	=					{Tier = 1000,Permission = {1163468836}},
+		["Giorno"] = 					{Tier = 1000,Permission = {3188670297}},
+		["Hakaishin"] = 				{Tier = 1000,Permission = {3188670297}},
+		["Boss"] = 					{Tier = 1000,Permission = {3188670297}},
+		["JP6"] = 					{Tier = 1000,Permission = {3188670297}},
+		["Gojo"] = 					{Tier = 1000,Permission = {3188670297,5131454597,3625209125}},
+		["Madara"] = 					{Tier = 1000,Permission = {3188670297}},
+	};
+};
